dfcc8f85fb6422a57cfff420c407a0815b9777d0
y1wtt <awataki1206@gmail.com>
2023-08-15 23:01:22 +0900

    Requestのテストコード追加

---
+++ src/test/kotlin/ChatGPTTest.kt
@@ -0,0 +1,14 @@
+import org.junit.jupiter.api.Assertions
+import org.junit.jupiter.api.Test
+import repository.chatai.ChatContext
+import repository.chatai.chatGPT.ChatGPTCompletionRequest
+
+class ChatGPTTest {
+	@Test
+	fun testRequestBody() {
+		Assertions.assertEquals(
+			"""{"model":"gpt-3.5-turbo","messages":[{"role":"user","content":"What is the OpenAI mission?"}]}""",
+			ChatGPTCompletionRequest("gpt-3.5-turbo", listOf(ChatContext("user","What is the OpenAI mission?"))).toJson()
+		)
+	}
+}
